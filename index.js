b98ce90ee714e78d0cfbd099298aa0cda1f0868c
EnzoAndresFarisCh <100326857+EnzoAndresFarisCh@users.noreply.github.com>
2024-05-30 03:28:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let browser
 let page
 
 async function startBrowser() {
-    browser = await puppeteer.launch({headless: true});
+    browser = await puppeteer.launch({headless: true,  executablePath: '/usr/bin/chromium-browser'});
     page = await browser.newPage();
     await page.goto('https://chatgpt.com/');
 }
