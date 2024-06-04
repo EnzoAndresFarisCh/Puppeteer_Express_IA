a086daad9ecb6eaea4729b7f03084e6a8078ad5d
EnzoAndresFarisCh <enzitofch@gmail.com>
2024-06-04 01:50:29 -0300

    ajuste

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import puppeteer from 'puppeteer';
 import dotenv from 'dotenv';
 dotenv.config();
 import bodyParser from 'body-parser'; 
+import cors from 'cors';
 
 const app = express();
 const port = 3001;
 let values = [];
+
+app.use(cors())
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/create-page', validateToken);
@@ -22,11 +26,11 @@ function validateToken(req, res, next) {
 
 app.post('/create-page', async (req, res) => {
     try {
-       let browser = await puppeteer.launch({headless: false,  executablePath: '/usr/bin/chromium-browser'});
+       let browser = await puppeteer.launch({headless: false, });
         let page = await browser.newPage();
         await page.goto('https://chatgpt.com/');
         const s = await page.waitForSelector("#prompt-textarea", { timeout: 50000 });
-
+        console.log(req)
         if (s) {
             let valorT;
             const s = setTimeout(() => {
@@ -65,44 +69,65 @@ app.post('/create-page', async (req, res) => {
 
 
 async function handlePageInteraction(page, text) {
-    const _v = process.env.seletor
-    await page.evaluate(async (text, seletor) => {
-        return new Promise(resolve => {
-            document.querySelector("#prompt-textarea").value = `${text},  com header, menu, main, footer, bem elaborado e com muito detalhe no html e css tudo num arquivo só, como code para copiar o codigo`;
-            document.querySelector("#prompt-textarea").dispatchEvent(new Event('input', { bubbles: true }));
-            setTimeout(() => {        
-                document.querySelector('button[data-testid="send-button"]').click();
-                resolve(true)
-            }, 1000);
-        });
-    },text, _v);
-
-    const valorT = await page.evaluate(() => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                let int = setInterval(() => {
-                    let elementos = document.querySelectorAll('div[data-message-author-role="assistant"]');
-                    if (elementos.length > 0) {
-                        let ultimoElemento = elementos[elementos.length - 1];
-                        let v = false;
-                        ultimoElemento.querySelectorAll('*').forEach(a => {
-                            estiloElemento = window.getComputedStyle(a, '::after');
-                            if (estiloElemento.content !== 'none' && estiloElemento.content == '"●"') {
-                                v = true;
+    try{
+        const _v = process.env.seletor
+        await page.evaluate(async (text, seletor) => {
+            return new Promise(resolve => {
+                const esperarSeletor = (s) => {
+                    return new Promise((response) => {
+                        let count = 0
+                        const interval = setInterval(() => {
+                            count++
+                            if(!!document.querySelector(s) || count > 1000){
+                                setTimeout(() => {
+                                    response(true)
+                                    clearInterval(interval)
+                                }, 500)
                             }
-                        });
-                        if (!v) {
-                            clearInterval(int);
-                            let value = !!elementos[elementos.length - 1].querySelector('code').textContent ? elementos[elementos.length - 1].querySelector('code').textContent : null;
-                            resolve(value)
-                        }
-                    }
+                        }, 100)
+                    })
+                }
+                document.querySelector("#prompt-textarea").value = `${text},  com header, menu, main, footer, bem elaborado e com muito detalhe no html e css tudo num arquivo só, como code para copiar o codigo`;
+                document.querySelector("#prompt-textarea").dispatchEvent(new Event('input', { bubbles: true }));
+                setTimeout(async () => {   
+                    esperarSeletor('button[data-testid="send-button"]')     
+                    let v = !!document.querySelector('button[data-testid="fruitjuice-send-button"]') ? document.querySelector('button[data-testid="fruitjuice-send-button"]') : document.querySelector('button[data-testid="send-button"]')
+                    v.click();
+                    resolve(true)
                 }, 1000);
-            }, 1000);
+            });
+        },text, _v);
+    
+        const valorT = await page.evaluate(() => {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    let int = setInterval(() => {
+                        let elementos = document.querySelectorAll('div[data-message-author-role="assistant"]');
+                        if (elementos.length > 0) {
+                            let ultimoElemento = elementos[elementos.length - 1];
+                            let v = false;
+                            ultimoElemento.querySelectorAll('*').forEach(a => {
+                                estiloElemento = window.getComputedStyle(a, '::after');
+                                if (estiloElemento.content !== 'none' && estiloElemento.content == '"●"') {
+                                    v = true;
+                                }
+                            });
+                             console.log(v)
+                            if (!v) {
+                                clearInterval(int);
+                                let value = !!elementos[elementos.length - 1].querySelector('code').textContent ? elementos[elementos.length - 1].querySelector('code').textContent : null;
+                                resolve(value)
+                            }
+                        }
+                    }, 1000);
+                }, 10000);
+            });
         });
-    });
-
-    return valorT;
+    
+        return valorT;
+    } catch (error) {
+        return null
+    }
 }
 
 function createToken(_token){
@@ -145,6 +170,8 @@ app.get('/status', (request, response) => {
 app.get('/get-values', (req, res) => {
     res.json(values);
 });
+
+app.use(express.static('front-dist'))
 
 app.listen(port, () => {
     console.log(`Servidor Express rodando em http://localhost:${port}`);
